36bfb44e99ee12f22cb1c8ffba2848f527b5f9e5
unknown <matheusfnx@hotmail.com>
2021-01-24 17:31:14 -0300

    Teste novo

--- ambiente_aws_data_lake/__init__.py
+++ ambiente_aws_data_lake/__init__.py
@@ -1,5 +1,5 @@
 import os
 
-from ambiente_data_lake.ambiente_aws_data_lake.environment import Environment
+from environment import Environment
 
 active_environment = Environment[os.environ['ENVIRONMENT']]
