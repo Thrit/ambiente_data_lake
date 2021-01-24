a3b8204b584ee5e32e8ae27f7415c9732a1f6107
unknown <matheusfnx@hotmail.com>
2021-01-24 16:58:32 -0300

    Solucionado caminho de classe no init.py

--- ambiente_aws_data_lake/__init__.py
+++ ambiente_aws_data_lake/__init__.py
@@ -1,4 +1,4 @@
 import os
-from environment import Environment
+from ambiente_data_lake.ambiente_aws_data_lake.environment import Environment
 
 active_environment = Environment[os.environ['ENVIRONMENT']]
